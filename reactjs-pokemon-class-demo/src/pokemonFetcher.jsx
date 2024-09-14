510ce52bb5d16d00882ae79f7b8e9b2de8485f52
hsc996 <han.scaife@gmail.com>
2024-09-14 11:39:19 +1000

    feat: componentDidMount with multiple data

--- reactjs-pokemon-class-demo/src/pokemonFetcher.jsx
+++ reactjs-pokemon-class-demo/src/pokemonFetcher.jsx
@@ -16,15 +16,26 @@ export default class PokemonFetcher extends React.Component {
 
     // Runs automatically once when the page first loads
     async componentDidMount(){
-        // Generate a random number for random pokemon number
-        let randomNumber = Math.floor(Math.random() * 1025) + 1;
 
-        // Pass random Pokemon number into fetch request
-        let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomNumber}`);
-        let data = await response.json();
+        for (let index = 0; index < 6; index++){
 
-        // Set fetch response data into state
-        this.setState({pokemonList: [data]});
+            // Generate a random number for random pokemon number
+            let randomNumber = Math.floor(Math.random() * 1025) + 1;
+
+            // Pass random Pokemon number into fetch request
+            let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomNumber}`);
+            let data = await response.json();
+
+            // Set fetch response data into state
+            // this.setState({pokemonList: [data]});
+            // this.setState({pokemonList: [...this.state.pokemonList, data]}); // Make a new array witht he combination of these 2 things
+            this.setState((previousState) => {
+                return {
+                    pokemonList: [...previousState.pokemonList, data]
+                }
+            }); // Modifying state from previousState allows you to modify from the latest version of the state -- this will protect your data, paritcularly useful will large datasets. Provides stability.
+        }
+
 
         console.log("PokemonFetcher first load on the page");
 
